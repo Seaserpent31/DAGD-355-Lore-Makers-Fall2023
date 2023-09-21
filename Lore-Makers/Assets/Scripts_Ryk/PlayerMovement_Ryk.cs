a08cb3f0a58bea81ac1d4839fb088c8c37987295
RykSacha14 <124813635+RykSacha14@users.noreply.github.com>
2023-09-21 14:38:18 -0400

    Created PlayerMovement

--- Lore-Makers/Assets/Scripts_Ryk/PlayerMovement_Ryk.cs
+++ Lore-Makers/Assets/Scripts_Ryk/PlayerMovement_Ryk.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBase_Ryk : MonoBehaviour
+public class PlayerMovement_Ryk : MonoBehaviour
 {
-    public int health;
-    public int damage;
-    public float speed;
-
-    public int maxHealth;
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
