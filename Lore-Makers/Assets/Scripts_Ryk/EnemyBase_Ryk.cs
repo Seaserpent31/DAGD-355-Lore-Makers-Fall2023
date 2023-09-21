ce86099263854707c3ec5906adfd7d9c25f0cae8
RykSacha14 <124813635+RykSacha14@users.noreply.github.com>
2023-09-21 14:23:04 -0400

    Created the Enemy Prefabs and Base script
    The base script has Health, Speed, and Damage for the moment

---
+++ Lore-Makers/Assets/Scripts_Ryk/EnemyBase_Ryk.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase_Ryk : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    public float speed;
+
+    public int maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
