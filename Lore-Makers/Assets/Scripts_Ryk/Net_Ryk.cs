2d420ce1118c66dadbd56da3f203f4c5a127df50
RykSacha14 <124813635+RykSacha14@users.noreply.github.com>
2023-09-26 15:38:15 -0400

    Created Sprite for Bombs

--- Lore-Makers/Assets/Scripts_Ryk/Net_Ryk.cs
+++ Lore-Makers/Assets/Scripts_Ryk/Net_Ryk.cs
@@ -8,10 +8,13 @@ public class Net_Ryk : MonoBehaviour
 
     public bool netActive = true;
 
+    public float burstRadius;
+
     // Start is called before the first frame update
     void Start()
     {
         speed = 4f;
+        burstRadius = 5f;
     }
 
     // Update is called once per frame
