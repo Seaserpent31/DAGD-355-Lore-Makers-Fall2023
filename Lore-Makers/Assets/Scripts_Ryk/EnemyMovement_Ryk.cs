a5e88183e5db964fb59d2777d776445f892b6890
RykSacha14 <124813635+RykSacha14@users.noreply.github.com>
2023-10-10 15:46:49 -0400

    GameManager
    Moving Code around to figure out a solution

--- Lore-Makers/Assets/Scripts_Ryk/EnemyMovement_Ryk.cs
+++ Lore-Makers/Assets/Scripts_Ryk/EnemyMovement_Ryk.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class EnemyMovement_Ryk : MonoBehaviour
 {
-    public float spawnPosX;
-    public float spawnPosY;
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnPosX = Random.Range(0, 10);
-        spawnPosY = Random.Range(0, 10);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(spawnPosX + " " + spawnPosY);
     }
 }
