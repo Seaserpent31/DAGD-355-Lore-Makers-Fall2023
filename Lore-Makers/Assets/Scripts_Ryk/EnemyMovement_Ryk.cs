041a6f8e14b91a3f68fb39dfc7a1d7a7191f1695
RykSacha14 <124813635+RykSacha14@users.noreply.github.com>
2023-10-14 18:42:39 -0400

    Updating Enemy AI
    Adding 1 Fire Pattern

--- Lore-Makers/Assets/Scripts_Ryk/EnemyMovement_Ryk.cs
+++ Lore-Makers/Assets/Scripts_Ryk/EnemyMovement_Ryk.cs
@@ -5,32 +5,57 @@ using UnityEngine;
 public class EnemyMovement_Ryk : MonoBehaviour
 {
     private int enemyFirePattern;
- 
+
+    private float enemyFireRate;
+
+    public GameObject shot;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        enemyFirePattern = Random.Range(0, 3);
+
+        //Debug.Log(spawnPosX + " " + spawnPosY);
+        Debug.Log(enemyFirePattern);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (enemyFirePattern == 0)
+        {
+            firePatternSingle();
+        }
+        else if (enemyFirePattern == 1)
+        {
+            firePatternSingle();
+        }
+        else if (enemyFirePattern == 2)
+        {
+            firePatternSingle();
+        }
+    }
+
+    private void firePatternSingle()
+    {
+        enemyFireRate += Time.deltaTime;
+        if (enemyFireRate >= 3 )
+        {
+            Instantiate(shot, GameObject.FindGameObjectWithTag("Enemy").transform.position, Quaternion.identity);
+            enemyFireRate = 0;
+        }
+    }
+
+    private void firePatternTriple()
+    {
+
+    }
+
+    private void firePatternPierce() 
+    {
         
-        enemyFirePattern = Random.Range(0, 3);
-        
-
-        if (enemyFirePattern == 0 )
-        {
-
-        }
-        else if (enemyFirePattern == 1 )
-        {
-
-        }
-        else if(enemyFirePattern == 2 )
-        {
-
-        }
-        //Debug.Log(spawnPosX + " " + spawnPosY);
     }
 }
