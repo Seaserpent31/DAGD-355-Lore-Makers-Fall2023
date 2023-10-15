1dc4e394fc7fc8a27a0cf3529ce9eee36db35d2c
lauren <laurencamgoodman@gmail.com>
2023-10-14 23:06:17 -0400

    Finished bullet patterns.
    (Still want to make better patterns - these are all incredibly simple)
    Still need to do collision with the enemies, different effects (for ex. poison darts need to do damage over time), etc.

--- Lore-Makers/Assets/Scripts_Lauren/Bullets_Lauren/BulletTypes_Lauren/Bullet_Lauren.cs
+++ Lore-Makers/Assets/Scripts_Lauren/Bullets_Lauren/BulletTypes_Lauren/Bullet_Lauren.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
 using UnityEngine.UIElements;
+using static UnityEditor.ObjectChangeEventStream;
+
 
 // Help used from:
 // https://youtu.be/Mq2zYk5tW_E?si=iDfJ1_zxp0sC6NAJ.
@@ -10,15 +14,13 @@ using UnityEngine.UIElements;
 // ==========[ BULLET ]==========
 // Basic bullet. Fires at a normal rate and deals a normal amount of damage to enemies hit.
 // To-Do:
-    // Mess around with numbers, add important things, etc. (same goes for every bullet script).
-
-
+// Mess around with numbers, add important things, etc. (same goes for every bullet script).
 
 // To fix my weapon sawpping, will probably have to have ALL bullet info on one script and swap between them.
-    // For ex) having a "Fire" and "Spiral" invoke but "if index == 0, fire" and "if index == 1, spiral" kind of thing.
-    // Kind of like swapping scenes/screens in DAGD 255.
+// For ex) having a "Fire" and "Spiral" invoke but "if index == 0, fire" and "if index == 1, spiral" kind of thing.
+// Kind of like swapping scenes/screens in DAGD 255.
 
-    // replace a lot of the numbers and stuff with variables so i can edit them for each bullet type.
+// replace a lot of the numbers and stuff with variables so i can edit them for each bullet type.
 #endregion
 
 public class Bullet_Lauren : MonoBehaviour
@@ -27,18 +29,25 @@ public class Bullet_Lauren : MonoBehaviour
     public WeaponManager_Lauren weaponManager;
     public GameManager gameManager;
 
-    [SerializeField] private int amountBullets = 10;
-    //[SerializeField] float startAngle = 270f, endAngle = 90f;
-    [SerializeField] float startAngle = 90f, endAngle = 270f;
-    // I want the bullets to shoot everywhere on screen.
+    // [SerializeField]
+    private int amountBullets;
+    // [SerializeField]
+    private float startAngle, endAngle;
 
-    [SerializeField] private float firerate;
+    // [SerializeField]
+    private float firerate;
+    private float speed;
+    private float angle;
 
     private Vector2 bulletMoveDirection;
 
-    // private bool isShooting = true;
+    private float lastShot = 0f;
 
-    private float angle = 0f;
+    public Transform target;
+
+    private Rigidbody2D rb;
+
+    // private bool isShooting = true;
 
     private int pattern = 0;
         // 0 - Normal
@@ -48,12 +57,10 @@ public class Bullet_Lauren : MonoBehaviour
         // 4 - Sniper
         // 5 - Speedy
 
-    private float lastShot = 0f;
-
     // ==========[ START ]==========
     private void Start()
     {
-        // StartShooting();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -97,75 +104,44 @@ public class Bullet_Lauren : MonoBehaviour
 
         switch (pattern)
         {
-            case 0:
-                Fire();
+            case 0: // Bullet
+                Bullet();
                 break;
-            case 1:
-                Spiral();
+            case 1: // Electro
+                Electro();
                 break;
-            case 2:
-                Fire();
+            case 2: // Poison Dart
+                PoisonDart();
                 break;
-            case 3:
-                Fire();
+            case 3: // Rocket
+                Rocket();
                 break;
-            case 4:
-                Fire();
+            case 4: // Sniper
+                Sniper();
                 break;
-            case 5:
-                Fire();
+            case 5: // Speedy Bullet
+                Speedy();
                 break;
         }
     }
 
-    // Fire() - Firing the bullet.
-    private void Fire()
+    // Bullet() - Shooting bullets.
+    private void Bullet()
     {
+        amountBullets = 8;
+        firerate = 5f;
+
         // So hundreds of bullets don't shoot at a time.
         float shotTime = 1f / firerate;
 
         if (Time.time - lastShot >= shotTime)
         {
             lastShot = Time.time;
-
-            float angleStep = (endAngle - startAngle) / amountBullets;
-            float angle = startAngle;
 
             for (int i = 0; i < amountBullets; i++)
             {
-                float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
-                float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
-                // Calculating end points.
-
-                Vector3 bulMove = new Vector3(bulDirX, bulDirY, 0f);
-                Vector2 bulDir = (bulMove - transform.position).normalized;
-
-                GameObject bullet = BulletPool_Lauren.instance.GetBullet();
-                bullet.transform.position = transform.position;
-                bullet.transform.rotation = transform.rotation;
-                bullet.SetActive(true);
-                bullet.GetComponent<BulletBase_Lauren>().SetDirection(bulDir);
-
-                angle += angleStep;
-            }
-        }
-
-    } // End of Fire().
-
-    // Spiral() - Spiral bullets.
-    private void Spiral()
-    {
-        // So hundreds of bullets don't shoot at a time.
-        float shotTime = 1f / firerate;
-
-        if (Time.time - lastShot >= shotTime)
-        {
-            lastShot = Time.time;
-
-            for (int i = 0; i < 10; i++)
-            {
-                float bulDirX = transform.position.x + Mathf.Sin(((angle + 90f * i) * Mathf.PI / 4) / 90f);
-                float bulDirY = transform.position.y + Mathf.Cos(((angle + 90f * i) * Mathf.PI / 4) / 90f);
+                float bulDirX = transform.position.x + Mathf.Sin((angle + i * (360 / amountBullets)) * Mathf.Deg2Rad);
+                float bulDirY = transform.position.y + Mathf.Cos((angle + i * (360 / amountBullets)) * Mathf.Deg2Rad);
 
                 Vector3 bulMove = new Vector3(bulDirX, bulDirY, 0f);
                 Vector2 bulDir = (bulMove - transform.position).normalized;
@@ -187,6 +163,246 @@ public class Bullet_Lauren : MonoBehaviour
             }
         }
 
-    } // End of Spiral().
+    } // End of Bullet().
+
+    // Electro() - Firing electro bullets.
+    private void Electro()
+    {
+        // Variables
+        amountBullets = 10;
+        startAngle = 0;
+        endAngle = 360;
+        firerate = 3f;
+
+        // So hundreds of bullets don't shoot at a time.
+        float shotTime = 1f / firerate;
+
+        if (Time.time - lastShot >= shotTime)
+        {
+            lastShot = Time.time;
+
+            // Angles
+            float angleStep = (endAngle - startAngle) / amountBullets;
+            float angle = startAngle;
+
+            for (int i = 0; i < amountBullets; i++)
+            {
+                float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180);
+                float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180);
+
+                Vector3 bulMove = new Vector3(bulDirX, bulDirY, 0f);
+                Vector2 bulDir = (bulMove - transform.position).normalized;
+
+                GameObject bullet = BulletPool_Lauren.instance.GetBullet();
+                bullet.transform.position = transform.position;
+                bullet.transform.rotation = transform.rotation;
+                bullet.SetActive(true);
+                bullet.GetComponent<BulletBase_Lauren>().SetDirection(bulDir);
+
+                angle += angleStep;
+            }
+        }
+
+    } // End of Electro().
+
+    // PoisonDart() - Firing poison darts.
+    private void PoisonDart()
+    {
+        amountBullets = 30;
+        firerate = 1f;
+
+        // So hundreds of bullets don't shoot at a time.
+        float shotTime = 1f / firerate;
+
+        if (Time.time - lastShot >= shotTime)
+        {
+            lastShot = Time.time;
+
+            for (int i = 0; i < amountBullets; i++)
+            {
+                float bulDirX = transform.position.x + Mathf.Sin((angle + i * (360 / amountBullets)) * Mathf.Deg2Rad);
+                float bulDirY = transform.position.y + Mathf.Cos((angle + i * (360 / amountBullets)) * Mathf.Deg2Rad);
+
+                Vector3 bulMove = new Vector3(bulDirX, bulDirY, 0f);
+                Vector2 bulDir = (bulMove - transform.position).normalized;
+
+                GameObject bul = BulletPool_Lauren.instance.GetBullet();
+                bul.transform.position = transform.position;
+                bul.transform.rotation = transform.rotation;
+                bul.SetActive(true);
+                bul.GetComponent<BulletBase_Lauren>().SetDirection(bulDir);
+
+            }
+
+            angle += 10f;
+
+            // If the angle reaches the "end," we have to reset it.
+            if (angle >= 360f)
+            {
+                angle = 0f;
+
+            }
+        }
+
+    } // End of PoisonDart().
+
+    // Rocket() - Firing rockets.
+    void Rocket()
+    {
+        amountBullets = 1;
+        firerate = 1f;
+        speed = 3f;
+        
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closestEnemy = null;
+
+        float closestDis = Mathf.Infinity;
+
+        float shotTime = 1f / firerate;
+
+        // For each enemy, find the distance of each to figure out which one is closest to the player.
+        foreach (GameObject enemy in enemies)
+        {
+            float disToEnemy = (enemy.transform.position - this.transform.position).sqrMagnitude;
+            if (disToEnemy < closestDis)
+            {
+                closestDis = disToEnemy;
+                closestEnemy = enemy;
+            }
+        }
+
+        // If there are no more enemies on screen, stop looking for the closest distance.
+        if (enemies.Length == 0)
+        {
+            return;
+        }
+
+        if (Time.time - lastShot >= shotTime)
+        {
+            lastShot = Time.time;
+
+            // If there is an enemy on screen, shoot at the closeset one.
+            if (closestEnemy != null)
+            {
+                // Find the direction of the enemy.
+                Vector3 directionToEnemy = (closestEnemy.transform.position - transform.position).normalized * speed;
+                // Vector2 bulDir = (directionToEnemy - transform.position).normalized;
+
+                // Shoot bullets towards that enemy's direction.
+                for (int i = 0; i < amountBullets; i++)
+                {
+                    GameObject bul = BulletPool_Lauren.instance.GetBullet();
+                    bul.transform.position = transform.position;
+                    bul.transform.rotation = transform.rotation;
+                    bul.SetActive(true);
+                    bul.GetComponent<BulletBase_Lauren>().SetDirection(directionToEnemy);
+                }
+
+            }
+            // Draw a line so we can see which enemy is closest (and to see if it worked).
+            Debug.DrawLine(transform.position, closestEnemy.transform.position);
+        }
+
+    } // End of Rocket().
+
+    // Sniper() - Firing sniper bullets.
+    private void Sniper()
+    {
+        amountBullets = 1;
+        firerate = 2f;
+        speed = 5f;
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closestEnemy = null;
+
+        float closestDis = Mathf.Infinity;
+
+        float shotTime = 1f / firerate;
+
+        // For each enemy, find the distance of each to figure out which one is closest to the player.
+        foreach (GameObject enemy in enemies)
+        {
+            float disToEnemy = (enemy.transform.position - this.transform.position).sqrMagnitude;
+            if (disToEnemy < closestDis)
+            {
+                closestDis = disToEnemy;
+                closestEnemy = enemy;
+            }
+        }
+
+        // If there are no more enemies on screen, stop looking for the closest distance.
+        if (enemies.Length == 0)
+        {
+            return;
+        }
+
+        if (Time.time - lastShot >= shotTime)
+        {
+            lastShot = Time.time;
+
+            // If there is an enemy on screen, shoot at the closeset one.
+            if (closestEnemy != null)
+            {
+                // Find the direction of the enemy.
+                Vector3 directionToEnemy = (closestEnemy.transform.position - transform.position).normalized * speed;
+                // Vector2 bulDir = (directionToEnemy - transform.position).normalized;
+
+                // Shoot bullets towards that enemy's direction.
+                for (int i = 0; i < amountBullets; i++)
+                {
+                    GameObject bul = BulletPool_Lauren.instance.GetBullet();
+                    bul.transform.position = transform.position;
+                    bul.transform.rotation = transform.rotation;
+                    bul.SetActive(true);
+                    bul.GetComponent<BulletBase_Lauren>().SetDirection(directionToEnemy);
+                }
+
+            }
+            // Draw a line so we can see which enemy is closest (and to see if it worked).
+            Debug.DrawLine(transform.position, closestEnemy.transform.position);
+        }
+
+    } // End of Sniper().
+
+    // Speedy() - Firing speedy bullets.
+    private void Speedy()
+    {
+        // Variables
+        amountBullets = 15;
+        startAngle = Random.Range(0f, 360f);
+        endAngle = Random.Range(0f, 360f);
+        firerate = 5f;
+        speed = 2f;
+        
+        // So hundreds of bullets don't shoot at a time.
+        float shotTime = 1f / firerate;
+
+        if (Time.time - lastShot >= shotTime)
+        {
+            lastShot = Time.time;
+
+            // Angles
+            float angleStep = (endAngle - startAngle) / amountBullets;
+            float angle = startAngle;
+
+            for (int i = 0; i < amountBullets; i++)
+            {
+                float bulDirX = transform.position.x + Mathf.Sin((angle + i * (360 / amountBullets)));
+                float bulDirY = transform.position.y + Mathf.Cos((angle - i * (360 / amountBullets)));
+
+                Vector3 bulMove = new Vector3(bulDirX, bulDirY, 0f);
+                Vector2 bulDir = (bulMove - transform.position).normalized * speed;
+
+                GameObject bullet = BulletPool_Lauren.instance.GetBullet();
+                bullet.transform.position = transform.position;
+                bullet.transform.rotation = transform.rotation;
+                bullet.SetActive(true);
+                bullet.GetComponent<BulletBase_Lauren>().SetDirection(bulDir);
+
+                angle += angleStep;
+            }
+        }
+
+    } // End of Speedy().
 
 } // End of Bullet.
